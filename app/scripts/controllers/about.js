78daac67840ca5a6812677ee749a408cce3fe801
Damien McMahon <damien.mcmahon@gmail.com>
2015-07-05 12:14:19 +1000

    Initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name checkItApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the checkItApp
+ */
+angular.module('checkItApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name checkItApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the checkItApp
+ */
+angular.module('checkItApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
